0d8d37efaa8c6ebf562e81f63722155d80e620fc
Azizbek Arzikulov <147023992+azizbekdevuz@users.noreply.github.com>
2024-06-11 10:55:49 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00D7"; // Unicode for '×'
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+
+    listContainer.querySelectorAll("li").forEach(li => {
+        li.querySelector("span").onclick = function() {
+            this.parentElement.remove();
+            saveData();
+        };
+        li.onclick = function() {
+            this.classList.toggle("checked");
+            saveData();
+        };
+    });
+}
+
+
+showTask();
